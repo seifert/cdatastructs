de144653ea8a03e1bd86de3f12bdf860a69801e2
Jan Seifert <jan.seifert@firma.seznam.cz>
2018-07-01 20:33:40 +0200

    export paths for building datastructs based packages

--- datastructs/__init__.py
+++ datastructs/__init__.py
@@ -1,2 +1,14 @@
+"""
+datastructs module provides simple data structures accessible from both
+Python and C.
+"""
 
 __version__ = '0.0.1'
+
+
+def get_c_sources_dir():
+    """
+    Return directory containig sources for building your own modules.
+    """
+    import os.path
+    return os.path.abspath(os.path.dirname(__file__))
